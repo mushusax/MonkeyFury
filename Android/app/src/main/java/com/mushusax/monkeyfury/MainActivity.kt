5c8f36bf81ea76fd700bd77253116f8df692198d
mushusax <mushu.aradhyula@gmail.com>
2025-05-11 14:47:28 -0500

    Adds LoginComposable

--- Android/app/src/main/java/com/mushusax/monkeyfury/MainActivity.kt
+++ Android/app/src/main/java/com/mushusax/monkeyfury/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.mushusax.monkeyfury.ui.theme.MonkeyFuryTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MonkeyFuryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { padding ->
+                    LoginComposable(modifier = Modifier
+                        .padding(padding)
+                        .fillMaxSize())
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MonkeyFuryTheme {
-        Greeting("Android")
+fun LoginComposable(modifier: Modifier) {
+    Column(modifier, verticalArrangement = Arrangement.Center) {
+        ElevatedButton(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Login", modifier = Modifier)
+        }
     }
 }
